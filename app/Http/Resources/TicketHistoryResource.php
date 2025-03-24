a5527c8594eb1cf9e4d84512e21743eb8dcc4356
amdymstfa <amdymoustapha011@gmail.com>
2025-03-24 12:12:59 +0000

    feat: add TicketHistoryResource to format ticket history changes

--- app/Http/Resources/TicketHistoryResource.php
+++ app/Http/Resources/TicketHistoryResource.php
@@ -14,6 +14,15 @@ class TicketHistoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'ticket_id' => $this->ticket_id,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'field_changed' => $this->field_changed,
+            'old_value' => $this->old_value,
+            'new_value' => $this->new_value,
+            'action_description' => $this->action_description,
+            'created_at' => $this->created_at,
+        ];
     }
 }
